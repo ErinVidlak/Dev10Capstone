f1131959e0041c7a16c9b95b7a1124fe02fc5aca
Rachael <rhazlett@dev-10.com>
2021-05-19 15:55:02 -0400

    Implement MaterialMapper

--- inventory-tracker-backend/src/main/java/learn/inventory/data/mappers/MaterialMapper.java
+++ inventory-tracker-backend/src/main/java/learn/inventory/data/mappers/MaterialMapper.java
@@ -1,16 +1,45 @@
 package learn.inventory.data.mappers;
 
 import learn.inventory.models.Material;
+import learn.inventory.models.User;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MaterialMapper implements RowMapper<Material> {
 
 
     @Override
     public Material mapRow(ResultSet resultSet, int i) throws SQLException {
-        return null;
+        Material material = new Material();
+
+        material.setMaterialId((resultSet.getInt("material_id")));
+        material.setMaterialName(resultSet.getString("material_name"));
+        material.setPricePerUnit(resultSet.getBigDecimal("price_per_unit"));
+
+        UserMapper userMapper = new UserMapper();
+        material.setUser(userMapper.mapRow(resultSet, i));
+
+        return material;
+
+
+
+//
+
+
+//
+//        Agent agent = new Agent();
+//        agent.setAgentId(resultSet.getInt("agent_id"));
+//        agent.setFirstName(resultSet.getString("first_name"));
+//        agent.setMiddleName(resultSet.getString("middle_name"));
+//        agent.setLastName(resultSet.getString("last_name"));
+//        if (resultSet.getDate("dob") != null) {
+//            agent.setDob(resultSet.getDate("dob").toLocalDate());
+//        }
+//        agent.setHeightInInches(resultSet.getInt("height_in_inches"));
+//        return agent;
     }
 }
