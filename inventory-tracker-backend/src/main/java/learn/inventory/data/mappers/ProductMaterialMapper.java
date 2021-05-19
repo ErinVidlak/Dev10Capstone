fe68046c3b0457b86f97538e98bf1f414e53b059
ErinVidlak <80711727+ErinVidlak@users.noreply.github.com>
2021-05-19 13:22:32 -0400

    Added boilerplate code for Mappers for ListedProduct, Platformfee, Product, & ProductMaterial

--- inventory-tracker-backend/src/main/java/learn/inventory/data/mappers/ProductMaterialMapper.java
+++ inventory-tracker-backend/src/main/java/learn/inventory/data/mappers/ProductMaterialMapper.java
@@ -1,4 +1,14 @@
 package learn.inventory.data.mappers;
 
-public class ProductMaterialMapper {
+import learn.inventory.models.ProductMaterial;
+import org.springframework.jdbc.core.RowMapper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ProductMaterialMapper implements RowMapper<ProductMaterial> {
+
+    @Override
+    public ProductMaterial mapRow(ResultSet resultSet, int i) throws SQLException {
+        return null;
+    }
 }
