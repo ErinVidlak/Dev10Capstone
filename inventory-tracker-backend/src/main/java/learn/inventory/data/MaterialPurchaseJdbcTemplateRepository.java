291b41fd7bdf1b6c427bf17a4c4ba18cff1f8927
rhazlett <rhazlett@dev-10.com>
2021-05-20 12:34:48 -0400

    Removed unused constant

--- inventory-tracker-backend/src/main/java/learn/inventory/data/MaterialPurchaseJdbcTemplateRepository.java
+++ inventory-tracker-backend/src/main/java/learn/inventory/data/MaterialPurchaseJdbcTemplateRepository.java
@@ -26,30 +26,12 @@ public class MaterialPurchaseJdbcTemplateRepository implements MaterialPurchaseR
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    //  private int purchaseId;
-//    private BigDecimal purchasePrice;
-//    private int quantityPurchased;
-//    private String units;
-//    private LocalDate datePurchased;
-//    private String description;
-//    private int materialId;
-    /*
-    material_purchase_id int primary key auto_increment,
-    purchase_price decimal (10,2) not null default 0.00,
-    purchase_quantity int not null,
-    quantity_units varchar(25),
-    purchase_date date,
-    purchase_description text,
-    material_id int not null,
-    constraint fk_material_purchase_material_id
-        foreign key (material_id)
-        references material(material_id)
-     */
+
     private final String PURCHASE_COL_NAMES = " material_purchase_id, purchase_price, purchase_quantity, quantity_units," +
             " purchase_date, purchase_description, material_id ";
-    private final String PURCHASE_COL_NAMES_ADD = " purchase_price, purchase_quantity, quantity_units," +
-            " purchase_date, purchase_description, material_id ";
 
+    
+    
     @Override
     public List<MaterialPurchase> findAll() {
         final String sql = "select " + PURCHASE_COL_NAMES + " from material_purchase limit 1000;";
