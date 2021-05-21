d8e00e30847ab0523397d3c4316b2756268d6821
Rachael <rhazlett@dev-10.com>
2021-05-21 17:00:50 -0400

    Update MaterialController.java

--- inventory-tracker-backend/src/main/java/learn/inventory/controllers/MaterialController.java
+++ inventory-tracker-backend/src/main/java/learn/inventory/controllers/MaterialController.java
@@ -1,6 +1,15 @@
 package learn.inventory.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = {"http://localhost:3000"})
+@RequestMapping("/api/material")
 public class MaterialController {
+
+ //   private final MaterialService service;
 
 
 }
