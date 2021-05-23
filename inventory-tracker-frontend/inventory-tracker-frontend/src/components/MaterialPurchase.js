5c305590d81b02bc16cca49188c876fa5fd5b799
ErinVidlak <80711727+ErinVidlak@users.noreply.github.com>
2021-05-23 12:16:36 -0400

    Created MaterialPurchase component

---
+++ inventory-tracker-frontend/inventory-tracker-frontend/src/components/MaterialPurchase.js
@@ -0,0 +1,17 @@
+
+function MaterialPurchase({ materialPurchase }) {
+
+    return (
+        <tr>
+            <td>
+                <input type="checkbox" checked="checked" />
+            </td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    );
+
+} 
+
+export default MaterialPurchase;
