d7e578d9f0c5fd45a8636b513306c0d08bec69a4
Rachael <rhazlett@dev-10.com>
2021-05-24 14:59:21 -0400

    Created utils directory
    Created utils to hold helper method classes

---
+++ inventory-tracker-frontend/inventory-tracker-frontend/src/utils/helpers.js
@@ -0,0 +1,10 @@
+export function formatPricePerUnit(pricePerUnit) {
+  return pricePerUnit.toFixed(2);
+}
+
+export function capitalizeEach(string) {
+  return string
+    .trim()
+    .toLowerCase()
+    .replace(/\w\S*/g, (w) => w.replace(/^\w/, (c) => c.toUpperCase()));
+}
