0cba7571b55670233297c48a458fb38644102d93
ErinVidlak <80711727+ErinVidlak@users.noreply.github.com>
2021-05-26 17:19:46 -0400

    Added capitalize each to material name in ProductMaterialTableSummary

--- inventory-tracker-frontend/inventory-tracker-frontend/src/components/product/ProductMaterialTableSummary.js
+++ inventory-tracker-frontend/inventory-tracker-frontend/src/components/product/ProductMaterialTableSummary.js
@@ -1,3 +1,5 @@
+import { capitalizeEach } from '../../utils/helpers';
+
 export default function ProductMaterialTableSummary({
     materialId,
     materialName,
@@ -9,7 +11,7 @@ export default function ProductMaterialTableSummary({
     return (
       <tr>
         <td>{materialId}</td>
-        <td>{materialName}</td>
+        <td>{capitalizeEach(materialName)}</td>
         <td>{materialQuantity}</td>
         <td>
         <button className="btn waves-effect waves-light btn teal accent-1 black-text" onClick={() => setShowPMUpdateForm({materialId, materialName, materialQuantity})}>Update</button>
