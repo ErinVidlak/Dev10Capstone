40510bb3e2d20e21e6b8cc1fcfd181c81926974e
ErinVidlak <80711727+ErinVidlak@users.noreply.github.com>
2021-05-27 09:43:58 -0400

    Gave material purchases a header

--- inventory-tracker-frontend/inventory-tracker-frontend/src/components/materialPurchase/MaterialPurchaseListView.js
+++ inventory-tracker-frontend/inventory-tracker-frontend/src/components/materialPurchase/MaterialPurchaseListView.js
@@ -22,33 +22,42 @@ export default function MaterialPurchaseListView() {
 
     return (
     <div className="container px-3 py-3">
-        <h3>View All Raw Material Purchases</h3>
-        <table className="striped">
-        <thead>
-            <tr>
-            <th>Purchase Date</th>
-            <th>Material Name</th>
-            <th>Cost</th>
-            <th>View</th>
-            </tr>
-        </thead>
+        <div className="row center">
+            <div className="card purple lighten-4">
+                <div className="card-content black-text">
+                    <span className="card-title center">Raw Material Purchases</span>
+                </div>
+            </div>
+        </div>
+        
+        <div className="row">
+            <table className="striped">
+                <thead>
+                    <tr>
+                    <th>Purchase Date</th>
+                    <th>Material Name</th>
+                    <th>Cost</th>
+                    <th>View</th>
+                    </tr>
+                </thead>
 
-        <tbody>
-            {materialPurchaseList.sort( (a, b) => {
-                return new Date(b.datePurchased) - new Date(a.datePurchased)
-            })
-            .map((materialPurchase) => (
-            <MaterialPurchaseSummary
-                key={materialPurchase.purchaseId}
-                datePurchased={materialPurchase.datePurchased}
-                materialId={materialPurchase.materialId}
-                purchasePrice={materialPurchase.purchasePrice}
-                materialPurchaseId={materialPurchase.purchaseId}
-            />
-            ))}
-        </tbody>
-        </table>
-        <button className="btn waves-effect waves-light btn-flat deep-purple lighten-3" onClick={addMaterialPurchase}>Add</button>
+                <tbody>
+                    {materialPurchaseList.sort( (a, b) => {
+                        return new Date(b.datePurchased) - new Date(a.datePurchased)
+                    })
+                    .map((materialPurchase) => (
+                    <MaterialPurchaseSummary
+                        key={materialPurchase.purchaseId}
+                        datePurchased={materialPurchase.datePurchased}
+                        materialId={materialPurchase.materialId}
+                        purchasePrice={materialPurchase.purchasePrice}
+                        materialPurchaseId={materialPurchase.purchaseId}
+                    />
+                    ))}
+                </tbody>
+            </table>
+        </div>
+        <button className="btn waves-effect waves-light btn teal accent-1 black-text" onClick={addMaterialPurchase}>Add a New Purchase</button>
         {messages.length > 0 && <Messages messages={messages}/>}
     </div>
     );
